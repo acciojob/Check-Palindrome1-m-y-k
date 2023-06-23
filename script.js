99498ebcea07ca8cbec6771163df5a8031f155b7
Mohammad Yusuf Khan <98419143+m-y-k@users.noreply.github.com>
2023-06-23 21:02:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-	let reverseStr = reverse(str);
-	return reverseStr === str;
+	return isPalindrome(str);
 }
 
 function reverse(str) {
@@ -12,4 +11,17 @@ function reverse(str) {
 	}
 	return ans;
 }
+
+
+function isPalindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const alphanumericStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  // Reverse the string
+  const reversedStr = alphanumericStr.split('').reverse().join('');
+
+  // Compare the original string with the reversed string
+  return alphanumericStr === reversedStr;
+}
+
 module.exports = palindrome
