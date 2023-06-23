1ce2ea69bf80f5e5ae5f73d2e05f6db5e40bff3b
Mohammad Yusuf Khan <98419143+m-y-k@users.noreply.github.com>
2023-06-23 20:59:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+	let reverseStr = reverse(str);
+	return reverseStr === str;
+}
 
+function reverse(str) {
+	let ans = "";
+	for (let i = 0; i < str.length; i++) {
+		ans = str.charAt(i) + ans;
+	}
+	return ans;
 }
 module.exports = palindrome
